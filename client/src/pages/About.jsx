2c6fd54e2fc1c56245dab0f2634a04e31fbfa2b1
Viet Lam <casau5duoikhoi@gmail.com>
2023-11-21 04:03:29 +0700

    feat: ui-search+home

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About us</h1>
+      <p className='mb-4 text-slate-700'>Bai tot nghiep PTIT - B19DCCN367</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem itaque, cupiditate tempora tenetur qui quasi laudantium, cum ipsam obcaecati doloribus pariatur, perspiciatis culpa quos dicta corrupti aut ducimus alias nemo.</p>
+      <p className='mb-4 text-slate-700'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos velit tempora excepturi inventore, harum molestiae totam est, ut nihil blanditiis asperiores culpa explicabo quibusdam, soluta porro. Esse laborum assumenda placeat.</p>
+    </div>
   )
 }
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -297,7 +297,9 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -314,7 +316,9 @@ export default function CreateListing() {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price</p>
-                  <span className='text-xs'>($ / month)</span>
+                  {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
                 </div>
               </div>
             )}
@@ -372,7 +376,7 @@ export default function CreateListing() {
             disabled={loading || uploading}
             className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
           >
-            {loading ? 'Creating...' : 'Update listing'}
+            {loading ? 'Updating...' : 'Update listing'}
           </button>
           {error && <p className='text-red-700 text-sm'>{error}</p>}
         </div>
